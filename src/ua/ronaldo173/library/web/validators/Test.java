9bee8ea896479a31258d74a1f3bafdebcefeaab0
Alexander <ronaldo173@inbox.ru>
2016-04-19 18:59:32 +0300

    download link with servlet made.

---
+++ src/ua/ronaldo173/library/web/validators/Test.java
@@ -0,0 +1,27 @@
+package ua.ronaldo173.library.web.validators;
+
+/**
+ * Created by Developer on 19.04.2016.
+ */
+public class Test {
+    public static void main(String[] args) {
+//        B b = new B();
+
+        A a = new B();
+    }
+}
+
+class A{
+    public A(){
+        print();
+    }
+    final void print(){
+        System.out.println("A");
+    }
+}
+
+class B extends A{
+     void print1() {
+        System.out.println("B");
+    }
+}
